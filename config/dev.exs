102716cd5cbd6addeb7f5d9c9c1295da71588a66
sobolevn <mail@sobolevn.me>
2019-03-06 10:46:03 +0300

    Removes webpack

--- config/dev.exs
+++ config/dev.exs
@@ -4,22 +4,13 @@ use Mix.Config
 # debugging and code reloading.
 #
 # The watchers configuration can be used to run external
-# watchers to your application. For example, we use it
-# with webpack to recompile .js and .css sources.
+# watchers to your application.
 config :kira, KiraWeb.Endpoint,
   http: [port: 4000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [
-    node: [
-      "node_modules/webpack/bin/webpack.js",
-      "--mode",
-      "development",
-      "--watch-stdin",
-      cd: Path.expand("../assets", __DIR__)
-    ]
-  ]
+  watchers: []
 
 # ## SSL Support
 #
