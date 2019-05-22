46ac2a51cfd0b6cd9ab799c077b3e2475e2a4e71
Artem Krivonozhko <artemkrivonozhko@gmail.com>
2019-05-22 13:58:28 +0300

    feat(wip): find task to assign service, refs #1

---
+++ lib/kira/projects/services/find_task_to_assign.ex
@@ -0,0 +1,13 @@
+defmodule Kira.Projects.Services.FindTaskToAssign do
+  @moduledoc """
+  Returns an open task with no assignee.
+  """
+
+  use Exop.Operation
+
+  # TODO: docs
+
+  def process(_params) do
+    # TODO
+  end
+end
---
+++ test/kira/projects/services/find_task_to_assign_test.exs
@@ -0,0 +1,30 @@
+defmodule KiraTest.Projects.Services.FindTaskToAssignTest do
+  use Kira.DataCase
+
+  import KiraTest.Factory
+  alias Kira.Projects.Services.FindTaskToAssign
+
+  # TODO:
+  # Find an issue with:
+  #   1. status: opened
+  #   2. assignee: nil
+  describe "find task to assign service" do
+    test "with no task to be assigned" do
+      # TODO
+    end
+
+    test "with only one task to be assigned" do
+      # TODO
+    end
+
+    test "with multiple tasks to be assigned" do
+      # test "without prioritization" do
+      # TODO
+      # end
+
+      # test "with prioritization" do
+      # TODO
+      # end
+    end
+  end
+end
