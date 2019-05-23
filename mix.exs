95fdb2069dcac80c602a22f88b886e4f8ab565ac
Nikita Mistyukov <nekifirus@gmail.com>
2019-05-23 23:24:36 +0500

    bump elixir version close#19

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Kira.MixProject do
     [
       app: :kira,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.8.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
