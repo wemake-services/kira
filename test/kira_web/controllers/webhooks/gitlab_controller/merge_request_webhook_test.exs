12cf1404ae93a56f2fcc8ea6361eaa7bc334bac8
Roman Solovjov <solovjov.roman@gmail.com>
2019-06-03 11:34:15 +0300

    handle close of merge request (#34)

--- test/kira_web/controllers/webhooks/gitlab_controller/merge_request_webhook_test.exs
+++ test/kira_web/controllers/webhooks/gitlab_controller/merge_request_webhook_test.exs
@@ -100,4 +100,51 @@ defmodule KiraWebTest.Webhooks.GitlabController.MergeRequestWebhookTest do
       assert instance.work_in_progress == false
     end
   end
+
+  describe "close merge request webhook" do
+    setup %{project: project, author: author} do
+      {:ok,
+       merge_request: insert(:merge_request, project: project, author: author)}
+    end
+
+    test "valid merge request data is handled", %{
+      merge_request: merge_request,
+      conn: conn
+    } do
+      created_at =
+        TimeUtils.to_gitlab_timeformat!(merge_request.origin_timestamp)
+
+      merge_request_payload =
+        merge_request
+        |> Map.from_struct()
+        |> Map.delete(:__meta__)
+        |> MapUtils.stringify_keys()
+        |> Map.merge(%{
+          "id" => merge_request.uid,
+          "author_id" => merge_request.author.uid,
+          "assignee_id" => nil,
+          "merge_status" => "can_be_merged",
+          "state" => "closed",
+          "action" => "close",
+          "work_in_progress" => false,
+          "created_at" => created_at
+        })
+
+      conn =
+        post(
+          conn,
+          Routes.gitlab_path(conn, :create),
+          %{
+            "object_kind" => "merge_request",
+            "project" => %{"id" => merge_request.project.uid},
+            "object_attributes" => merge_request_payload
+          }
+        )
+
+      assert response(conn, 200)
+
+      instance = MergeRequestQueries.get_merge_request!(merge_request.uid)
+      assert instance.state == "closed"
+    end
+  end
 end
